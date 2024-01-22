699c4598b5883859ea332aa555ef2b8172cc1ec3
John Blischak <jdblischak@gmail.com>
2024-01-22 16:38:13 -0500

    Fix directory for R build script

--- recipe/build-tiledb-r.sh
+++ recipe/build-tiledb-r.sh
@@ -1,6 +1,8 @@
 set -x
 set -o xtrace
 
+cd TileDB-R/
+
 ## Build a tarball and install from the tarball is the officially blessed way
 ## It has also respects the .Rbuildignore file allowing us to fine-tune
 $R CMD build --no-manual --no-build-vignettes .
