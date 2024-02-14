f2921af1f712498bab2727ce43dcc148525aea22
Ubuntu <ubuntu@ip-172-31-43-56.eu-central-1.compute.internal>
2024-02-14 13:27:34 +0000

    Enable caching for R

--- recipe/enable-caching.sh
+++ recipe/enable-caching.sh
@@ -6,3 +6,7 @@ export NN_CXX_ORIG=$CXX
 export NN_CC_ORIG=$CC
 export CXX=$RECIPE_DIR/cxx_wrap.sh
 export CC=$RECIPE_DIR/cc_wrap.sh
+
+mkdir -p ~/.R
+echo -e "CC=$CC\nCXX=$CXX\nCXX14=$CXX\nCXX17=$CXX\nCXX20=$CXX\n" > ~/.R/Makevars
+cat ~/.R/Makevars
